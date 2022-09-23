35e4fc3b0f468bf93646c08db83206ce5869878a
580dev <dansteve580@gmail.com>
2022-09-23 10:14:43 +0100

    scroll correct

--- www/assets/js/horizontal-scroll-eda.js
+++ www/assets/js/horizontal-scroll-eda.js
@@ -1,10 +1,11 @@
 // duration of scroll animation
 var scrollDuration = 300;
 // paddles
-var leftPaddle = document.getElementsByClassName('left-paddle');
-var rightPaddle = document.getElementsByClassName('right-paddle');
+var leftPaddle = document.getElementById('left-paddle');
+var rightPaddle = document.getElementById('right-paddle');
 // get items dimensions
 var itemsLength = $('.itemVariable').length;
+console.log(itemsLength)
 var itemSize = $('.itemVariable').outerWidth(true);
 console.log("=================== itemSize =============");
 console.log(itemSize);
@@ -12,9 +13,10 @@ console.log(itemSize);
 var paddleMargin = 20;
 
 // get wrapper width
-var getfWrapperSize = function() {
+var getMenuWrapperSize = function() {
 	return $('.menu-wrapper').outerWidth();
 }
+
 var menuWrapperSize = getMenuWrapperSize();
 // the wrapper is responsive
 $(window).on('resize', function() {
@@ -26,7 +28,7 @@ var menuVisibleSize = menuWrapperSize;
 
 // get total width of all menu items
 var getMenuSize = function() {
-	return itemsLength * itemSize + 270;
+	return itemsLength * itemSize + 305;
 };
 var menuSize = getMenuSize();
 // get how much of menu is invisible
@@ -49,18 +51,26 @@ $('.menuVariable').on('scroll', function() {
 
 	// show & hide the paddles 
 	// depending on scroll position
+
+    console.log(menuPosition +" == " + paddleMargin)
+    console.log(menuPosition <= paddleMargin)
+    console.log(menuPosition + 100 +" == " + menuEndOffset)
+    console.log(menuPosition + 100 < menuEndOffset)
+    console.log(menuPosition + 100 +" == " + menuEndOffset)
+    console.log(menuPosition + 100 >= menuEndOffset)
+
 	if (menuPosition <= paddleMargin) {
 		$(leftPaddle).addClass('hidden');
 		$(rightPaddle).removeClass('hidden');
-	} else if (menuPosition + 250 < menuEndOffset) {
+	} else if (menuPosition + 100 < menuEndOffset) {
 		// show both paddles in the middle
 		$(leftPaddle).removeClass('hidden');
 		$(rightPaddle).removeClass('hidden');
-	} else if (menuPosition + 250 >= menuEndOffset) {
+	} else if (menuPosition + 100 >= menuEndOffset) {
         console.log("=========================== close end")
 		$(leftPaddle).removeClass('hidden');
 		$(rightPaddle).addClass('hidden');
-}
+    }
 
 });
 
@@ -68,13 +78,14 @@ let scrollPosition = 0;
 
 // scroll to left
 $(rightPaddle).on('click', function() {
+    
     scrollPosition += itemSize;
 	$('.menuVariable').animate( { scrollLeft: scrollPosition}, scrollDuration);
 });
 
 // scroll to right
 $(leftPaddle).on('click', function() {
-	console.log('Test');
+	
     scrollPosition -= itemSize;
 	$('.menuVariable').animate( { scrollLeft: scrollPosition }, scrollDuration);
 	

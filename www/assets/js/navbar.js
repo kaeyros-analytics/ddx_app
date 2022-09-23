ebfd7b673b15bf22ca0cf004838ab0b17100bf88
580dev <dansteve580@gmail.com>
2022-09-23 14:38:18 +0100

    side change

--- www/assets/js/navbar.js
+++ www/assets/js/navbar.js
@@ -1,22 +1,24 @@
-// $(document).ready(function(){
-//     var $window = $(window);
-//       $('section[data-type="background"]').each(function(){
-//           var $bgobj = $(this); // assigning the object
-      
-//           $(window).scroll(function() {
-//               var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-              
-//               // Put together our final background position
-//               var coords = '50% '+ yPos + 'px';
-  
-//               // Move the background
-//               $bgobj.css({ backgroundPosition: coords });
-//           }); 
-//       });    
-//   });
-  
   //menu transition js
   $(document).ready(function(){
+    $('#form-1').show();
+    $('#pills-home-tab').on('click', function() {
+      $('#form-1').show();
+      $('#form-2').hide();
+      $('#form-3').hide();
+    });
+
+    $('#pills-profile-tab').on('click', function() {
+      $('#form-2').show();
+      $('#form-1').hide();
+      $('#form-3').hide();
+    });
+
+    $('#pills-contact-tab').on('click', function() {
+      $('#form-3').show();
+      $('#form-1').hide();
+      $('#form-2').hide();
+    });
+
     $('.x-logo-white').hide();
     $(".nav-link").css("font-weight", "bold");
     $(".nav-link").css("color", "white");
